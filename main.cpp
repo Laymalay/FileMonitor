83808279b73712e278ffb673a3e46769d64a2296
ALina <laymalay@gmail.com>
2017-10-17 11:23:47 +0300

    Add opacity to mainwindow

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowOpacity(0.95);
     w.setWindowTitle("File System Manager");
     w.show();
     //w.showFullScreen();
