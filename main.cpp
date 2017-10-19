fd825c221fa66207f08c8cb559698199182e4f5c
ALina <laymalay@gmail.com>
2017-10-19 09:58:06 +0300

    add <show maxmized>

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setWindowOpacity(0.95);
     w.setWindowTitle("File System Manager");
-    w.show();
+    //w.show();
+    w.showMaximized();
     //w.showFullScreen();
     return a.exec();
 }
